3fa76b8c4c107f526b2a8ee81bf6aec5724b93a8
MonacoMV <90590179+MonacoMV@users.noreply.github.com>
2022-09-24 13:52:35 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo pacman -Syy --noconfirm --needed
 
 echo "Installing Packages"
 
-PACKAGES="alacritty bspwm sxhkd rofi feh vim xorg-xinit xorg-server xorg-xrandr ttf-font-awesome polybar adobe-source-code-pro-fonts vim python3"
+PACKAGES="alacritty bspwm sxhkd rofi feh vim xorg-xinit xorg-server xorg-xrandr ttf-font-awesome polybar adobe-source-code-pro-fonts vim python3 xorg-xset"
 
 sudo pacman -S --noconfirm --needed $PACKAGES
 
