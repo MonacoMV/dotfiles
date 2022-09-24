1e4240f2821d94fc42ba4e22b8c28cffb58ea486
MonacoMV <90590179+MonacoMV@users.noreply.github.com>
2022-09-24 13:58:20 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -7,12 +7,14 @@ sudo rm -rf ~/.vimrc > /dev/null 2>&1
 sudo rm -rf ~/.bashrc > /dev/null 2>&1
 sudo rm -rf ~/.config > /dev/null 2>&1
 sudo rm -rf ~/.xinitrc > /dev/null 2>&1
+sudo rm -rf ~/wallpaper > /dev/null/ 2>&1
 
 ln -sf $dotfiles_dir/vim ~/.vim
 ln -sf $dotfiles_dir/vimrc ~/.vimrc
 ln -sf $dotfiles_dir/bashrc ~/.bashrc
 ln -sf $dotfiles_dir/config ~/.config
-ln -sf $dotfiles_dir/ ~/.xinitrc
+ln -sf $dotfiles_dir/xinitrc ~/.xinitrc
+ln -sf $dotfiles_dir/wallpaper
 
 chmod +x $dotfiles_dir/config/polybar/launch.sh
 chmod +x $dotfiles_dir/config/sxhkd/sxhkdrc
